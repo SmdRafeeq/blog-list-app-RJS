24649fd7e5f351e23edd9986d8deb9e7b4eb927e
MdRafeeq <smrafeeq780@gmail.com>
2023-07-28 06:32:46 +0000

    Blog list app test

---
+++ src/components/BlogItem/index.js
@@ -0,0 +1,19 @@
+import './index.css'
+
+const BlogItem = props => {
+  const {blogDetails} = props
+  const {title, description, publishedDate} = blogDetails
+
+  return (
+    <li className="blog-item">
+      <div className="first-div">
+        <h1 className="title">{title}</h1>
+        <p className="date">{publishedDate}</p>
+      </div>
+      <p className="description">{description}</p>
+      <hr />
+    </li>
+  )
+}
+
+export default BlogItem
---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+
+const UserInfo = () => (
+  <div className="user-info-con">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+      alt="profile"
+      className="user-profile"
+    />
+    <h1 className="user-name">Wade Warren</h1>
+    <p className="designation">Software developer at UK</p>
+  </div>
+)
+
+export default UserInfo
